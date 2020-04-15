799905d077f97a91cc9c72ddc85068b4be8003f3
northwestwitch <rasi.chiara@gmail.com>
2020-04-15 15:04:28 +0200

    add database delete command to the cli

---
+++ cgbeacon2/utils/delete.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+import logging
+
+LOG = logging.getLogger(__name__)
+
+def delete_dataset(mongo_db, id):
+    """Delete a dataset from dataset collection
+
+    Accepts:
+        id(str): dataset id
+
+    Returns:
+        result.deleted(int): number of deleted documents
+    """
+
+    collection = "dataset"
+
+    try:
+        result = mongo_db[collection].delete_one({"_id":id})
+    except Exception as err:
+        LOG.error(err)
+        return
+    return result.deleted_count
---
+++ tests/cli/delete/test_delete_dataset.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+import click
+
+from cgbeacon2.cli.commands import cli
+
+def test_delete_non_existing_dataset(mock_app):
+    """Test the command to delete a dataset when dataset doesn't exist"""
+
+    # test add a dataset_obj using the app cli
+    runner = mock_app.test_cli_runner()
+
+    # When invoking the command without the -id parameter
+    result =  runner.invoke(cli, [
+        'delete',
+        'dataset',
+        '-id', 'foo'
+        ])
+
+    # Then the command should run
+    assert result.exit_code == 0
+
+    # and return a warning
+    assert "Coundn't find a dataset with id 'foo' in database" in result.output
+
+def test_delete_existing_dataset(test_dataset_cli, mock_app, database):
+    """Test the command line to delete an existing dataset"""
+
+    # test add a dataset_obj using the app cli
+    runner = mock_app.test_cli_runner()
+
+    dataset = test_dataset_cli
+
+    # When a dataset is inserted intp database
+    result =  runner.invoke(cli, [
+       'add',
+       'dataset',
+       '-id', dataset["_id"],
+       '-name', dataset["name"],
+       ])
+
+    new_dataset = database["dataset"].find_one()
+    assert new_dataset is not None
+
+    # If the dataset delete command is invoved providing the right database id
+    result =  runner.invoke(cli, [
+       'delete',
+       'dataset',
+       '-id', new_dataset["_id"],
+       ])
+
+    # Then the command should be executed with no errors
+    assert result.exit_code == 0
+    assert "Dataset was successfully deleted" in result.output
+
+    # And the dataset should have been removed from the database
+    new_dataset = database["dataset"].find_one()
+    assert new_dataset is None
