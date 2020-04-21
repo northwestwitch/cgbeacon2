6c65c52888ce529ef07583d48b946cae1c355e48
northwestwitch <rasi.chiara@gmail.com>
2020-04-21 11:33:51 +0200

    added a test

--- tests/cli/delete/test_delete_variants.py
+++ tests/cli/delete/test_delete_variants.py
@@ -2,6 +2,7 @@
 import click
 
 from cgbeacon2.cli.commands import cli
+from cgbeacon2.resources import test_snv_vcf_path
 
 def test_delete_variants_confirm(mock_app):
     """Test confirm question in delete variants command"""
@@ -55,3 +56,50 @@ def test_delete_variants_non_existing_sample(mock_app, test_dataset_cli, databas
 
     assert result.exit_code == 1
     assert  "Couldn't find any sample 'bar' in the sample list of dataset" in result.output
+
+
+def test_delete_variants(mock_app, test_dataset_cli, database):
+    """Test the command to delete variants"""
+
+    runner = mock_app.test_cli_runner()
+
+    # Having a database containing a dataset
+    dataset = test_dataset_cli
+    database["dataset"].insert_one(dataset)
+
+    # And a variants from 2 different samples of a dataset
+    sample = "ADM1059A1"
+    sample2 = "ADM1059A2"
+
+    result = runner.invoke(
+        cli,
+        [
+            "add",
+            "variants",
+            "-ds",
+            dataset["_id"],
+            "-vcf",
+            test_snv_vcf_path,
+            "-sample",
+            sample,
+            "-sample",
+            sample2,
+        ],
+    )
+    initial_vars = sum(1 for i in database["variant"].find())
+    assert initial_vars > 0
+
+    # Then when using the cli command to remove onw of the samples
+    result = runner.invoke(cli, [
+        "delete",
+        "variants",
+        "-ds", test_dataset_cli["_id"],
+        "-sample", sample
+    ], input='y\n')
+
+    # Then there should be less variants left in the database
+    remaining_vars = sum(1 for i in database["variant"].find())
+    assert remaining_vars > 0
+    assert remaining_vars < initial_vars
+
+    
