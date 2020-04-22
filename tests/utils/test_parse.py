1376d490cb05a4bf64424eee0c561284019f4bd6
northwestwitch <rasi.chiara@gmail.com>
2020-04-22 10:28:24 +0200

    added pybedtools functions to merge gene panels

---
+++ tests/utils/test_parse.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import pybedtools
+from cgbeacon2.resources import panel1_path, panel2_path
+from cgbeacon2.utils.parse import merge_intervals
+
+def test_merge_intervals():
+    """Test function using pyBedTools for merging intervals from one or more panels"""
+
+    a = pybedtools.example_bedtool('a.bed') # path to test file a
+    """ This file looks like this:
+    chr1	1	100	feature1	0	+
+    chr1	100	200	feature2	0	+
+    chr1	150	500	feature3	0	-
+    chr1	900	950	feature4	0	+
+    """
+    assert len(a) == 4
+
+    b = pybedtools.example_bedtool('b.bed') # path to test file b
+    """ This file looks like this:
+    chr1	155	200	feature5	0	-
+    chr1	800	901	feature6	0	+
+    """
+    assert len(b) == 2
+
+    merged_bed = merge_intervals([a, b])
+    assert len(merged_bed) == 2
+    """Merged file looks like this:
+    chr1	1	500
+    chr1	800	950
+    """
