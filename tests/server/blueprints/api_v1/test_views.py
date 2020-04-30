9d409a5e1b20f93729ceadbd80f27432112f334e
northwestwitch <rasi.chiara@gmail.com>
2020-04-30 09:18:32 +0200

    include tests for queries with and without matches (HIT & MISS)

--- tests/server/blueprints/api_v1/test_views.py
+++ tests/server/blueprints/api_v1/test_views.py
@@ -161,7 +161,74 @@ def test_get_request_exact_position_snv_return_ALL(
     # Beacon info should be returned
     assert data["beaconId"]
     assert data["apiVersion"] == "1.0.0"
-    assert data["datasetAlleleResponses"]
+
+    # And both types of responses should be returned (exists=True and exists=False)
+    assert len(data["datasetAlleleResponses"]) == 2
+
+
+def test_get_request_exact_position_snv_return_HIT(
+    mock_app, test_snv, test_dataset_cli, test_dataset_no_variants
+):
+    """Test the query endpoint by sending a GET request. Search for SNVs, exact position, return only responses from dataset with variant (HIT)"""
+
+    # Having a dataset with a variant:
+    database = mock_app.db
+    database["variant"].insert_one(test_snv)
+
+    # And 2 datasets
+    test_dataset_cli["samples"] = ["ADM1059A1"]
+    for ds in [test_dataset_cli, test_dataset_no_variants]:
+        database["dataset"].insert_one(ds)
+
+    # when providing the required parameters in a SNV query for exact positions
+    ds_reponse_type = "includeDatasetResponses=HIT"
+    query_string = "&".join([BASE_ARGS, COORDS_ARGS, ALT_ARG, ds_reponse_type])
+
+    response = mock_app.test_client().get("".join(["/apiv1.0/", query_string]))
+    data = json.loads(response.data)
+
+    # No error should be returned
+    assert response.status_code == 200
+    assert data.get("message") is None
+
+    # And only the dataset with hits should be returned
+    assert len(data["datasetAlleleResponses"]) == 1
+    assert data["datasetAlleleResponses"][0]["datasetId"] == test_dataset_cli["_id"]
+    assert data["datasetAlleleResponses"][0]["exists"] == True
+
+
+def test_get_request_exact_position_snv_return_MISS(
+    mock_app, test_snv, test_dataset_cli, test_dataset_no_variants
+):
+    """Test the query endpoint by sending a GET request. Search for SNVs, exact position, return only responses from dataset with no hots (MISS)"""
+
+    # Having a dataset with a variant:
+    database = mock_app.db
+    database["variant"].insert_one(test_snv)
+
+    # And 2 datasets
+    test_dataset_cli["samples"] = ["ADM1059A1"]
+    for ds in [test_dataset_cli, test_dataset_no_variants]:
+        database["dataset"].insert_one(ds)
+
+    # when providing the required parameters in a SNV query for exact positions
+    ds_reponse_type = "includeDatasetResponses=MISS"
+    query_string = "&".join([BASE_ARGS, COORDS_ARGS, ALT_ARG, ds_reponse_type])
+
+    response = mock_app.test_client().get("".join(["/apiv1.0/", query_string]))
+    data = json.loads(response.data)
+
+    # No error should be returned
+    assert response.status_code == 200
+    assert data.get("message") is None
+
+    # And only the dataset with NO hits should be returned
+    assert len(data["datasetAlleleResponses"]) == 1
+    assert (
+        data["datasetAlleleResponses"][0]["datasetId"]
+        == test_dataset_no_variants["_id"]
+    )
+    assert data["datasetAlleleResponses"][0]["exists"] == False
 
 
 ################## TESTS FOR HANDLING SV REQUESTS ################
