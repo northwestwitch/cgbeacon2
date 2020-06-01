ab99905bb532b8d0399af51260500872b66b2308
northwestwitch <rasi.chiara@gmail.com>
2020-06-01 11:34:19 +0200

    increase coverage

---
+++ tests/utils/test_add.py
@@ -0,0 +1,17 @@
+from cgbeacon2.utils.add import add_dataset
+
+
+def test_add_dataset_twice(public_dataset, database):
+    """Test that the add_dataset function exists when the same dataset is added twice"""
+
+    # WHEN a dataset is first saved to database
+    result = add_dataset(database, public_dataset)
+
+    # The function should return a non-None document ID
+    assert result is not None
+
+    # WHEN the same function is called again to save the same database
+    result = add_dataset(database, public_dataset)
+
+    # THEN it should exit and return None
+    assert result is None
